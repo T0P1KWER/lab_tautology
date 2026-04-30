2bb5ec172f98928703d37b20d4c5c222fe4d9f20
Александр Сергеевич Вашкевич <42xxjjgydg@privaterelay.appleid.com>
2026-04-30 15:05:23 +0300

    Delete src/operand.py

--- src/operand.py
+++
@@ -1,9 +0,0 @@
-from src.tree_node import TreeNode
-
-
-class Operand(TreeNode):
-    def __init__(self, name: str):
-        self._name = name
-
-    def solve(self, values: dict[str, int]) -> int:
-        return values[self._name]
